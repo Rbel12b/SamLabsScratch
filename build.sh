52af7858246b9008814b1c77c6a95f3f612ad66d
Rbel12b <rbel12b@gmail.com>
2025-05-07 09:31:24 +0200

    Update build script to include cloning of tm2scratch and adjust build process

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+git clone https://github.com/rbel12b/tm2scratch scratch-gui/tm2scratch --depth=1
+
 cd ./scratch-samlabs && npm run build
-cd ../scratch-gui && npm run build
+
+cd ../scratch-gui 
+sh ./tm2scratch/install.sh
+npm run build
 cd ..
-cp -r ./scratch-samlabs/dist ./Scratch
-cp -r ./scratch-gui/build/* ./Scratch
+
+cp -r ./scratch-samlabs/dist ./scratch-gui/build
+
+cd scratch-gui
+rm -rf node_modules/scratch-vm/src/extensions/scratch3_tm2scratch node_modules/scratch-vm/src/extension-support/extension-manager.js
+mv node_modules/scratch-vm/src/extension-support/extension-manager.js_orig node_modules/scratch-vm/src/extension-support/extension-manager.js
+
+rm -rf src/lib/libraries/extensions/tm2scratch src/lib/libraries/extensions/index.jsx
+mv src/lib/libraries/extensions/index.jsx_orig src/lib/libraries/extensions/index.jsx
+
+rm -rf ./tm2scratch
